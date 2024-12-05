3cfd8d9ac307beda0e9e64503bfe6e355be647a6
Priyansh <priyanshdev09@gmail.com>
2024-12-05 14:07:21 +0530

    5/12/24

---
+++ variable/swap with 3 variable.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,temp;
+    cout << "Enter first number";
+    cin >> a;
+    cout << "Enter the second number";
+    cin >> b;
+    
+    //swapping 
+    temp=a;
+    a=b;
+    b=temp;
+    
+    cout << "value of a is : " << a ;
+    cout << "value of b is : " << b ;
+
+
+}
